7dd1e5a61b10b449aa47d7b9726de7f94ed4e898
moripen <markusmoripen@gmail.com>
2023-03-24 14:21:19 +0100

    feat: fields completed

--- schemas/timely.ts
+++ schemas/timely.ts
@@ -35,7 +35,39 @@ export default {
       type: 'string',
       title: 'Timely Account ID',
       description: `
-      Masse info.
+        The account ID of the Timely account.
+      `
+    },
+    {
+      name: 'timelyToken',
+      type: 'string',
+      title: 'Timely Token',
+      description: `
+        The API token of the Timely account.
+      `
+    },
+    {
+      name: 'timelyDefaultClient',
+      type: 'number',
+      title: 'Timely Default Client',
+      description: `
+        The default client to use when creating new Timely projects.
+      `
+    },
+    {
+      name: 'tripletexActivityCode',
+      type: 'number',
+      title: 'Tripletex Activity Code',
+      description: `
+        The activity code to use when creating new Tripletex time entries.
+      `
+    },
+    {
+      name: 'employeeToken',
+      type: 'string',
+      title: 'Employee Token',
+      description: `
+        The employee token to use when creating new Tripletex time entries.
       `
     },
     {
